fd1d099f2e05e4130567d2930158f206a33eeafb
vdins <71593118+vdins@users.noreply.github.com>
2025-03-03 09:57:13 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+     export default function Home() {
+       return (
+         <div>
+           <h1>Selamat Datang di Aplikasi Catur Online</h1>
+           <p>Website sedang dalam pengembangan...</p>
+         </div>
+       )
+     }
